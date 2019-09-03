6e69ada5cee86022920a4b57914c6b7908a67db6
feiyuhuahuo <32631344+feiyuhuahuo@users.noreply.github.com>
2019-09-03 19:41:30 +0800

    Add files via upload

---
+++ data/coco.py
@@ -0,0 +1,137 @@
+import os.path as osp
+import torch
+import torch.utils.data as data
+import cv2
+import numpy as np
+from .config import COCO_LABEL_MAP
+from pycocotools.coco import COCO
+
+def detection_collate(batch):
+    imgs = []
+    targets = []
+    masks = []
+    num_crowds = []
+
+    for sample in batch:
+        imgs.append(sample[0])
+        targets.append(torch.FloatTensor(sample[1]))
+        masks.append(torch.FloatTensor(sample[2]))
+        num_crowds.append(sample[3])
+
+    return torch.stack(imgs, 0), targets, masks, num_crowds
+
+class COCODetection(data.Dataset):
+    """MS Coco Detection <http://mscoco.org/dataset/#detections-challenge2016>_ Dataset."""
+    def __init__(self, image_path, info_file, augmentation=None):
+        self.image_path = image_path
+        self.coco = COCO(info_file)
+
+        self.ids = list(self.coco.imgToAnns.keys())
+
+        if len(self.ids) == 0:
+            self.ids = list(self.coco.imgs.keys())
+        
+        self.augmentation = augmentation
+
+    @staticmethod
+    def annotation_transform(target, width, height):
+        """
+        Args:
+            target (dict): COCO target json annotation as a python dict
+            height (int): height
+            width (int): width
+        Returns:
+            a list containing lists of bounding boxes  [bbox coords, class idx]
+        """
+        scale = np.array([width, height, width, height])
+        res = []
+        for obj in target:
+            if 'bbox' in obj:
+                bbox = obj['bbox']
+                label_idx = COCO_LABEL_MAP[obj['category_id']] - 1
+                final_box = list(np.array([bbox[0], bbox[1], bbox[0] + bbox[2], bbox[1] + bbox[3]]) / scale)
+                final_box.append(label_idx)
+                res += [final_box]    # [xmin, ymin, xmax, ymax, label_idx]
+            else:
+                print("No bbox found for object ", obj)
+
+        return res
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+        Returns:
+            tuple: Tuple (image, (target, masks, num_crowds)).
+                   target is the object returned by ``coco.loadAnns``.
+        """
+        im, gt, masks, h, w, num_crowds = self.pull_item(index)
+        return im, gt, masks, num_crowds
+
+    def __len__(self):
+        return len(self.ids)
+
+    def pull_item(self, index):
+        """
+        Args:
+            index (int): Index
+        Returns:
+            tuple: Tuple (image, target, masks, height, width, crowd).
+                   target is the object returned by ``coco.loadAnns``.
+            Note that if no crowd annotations exist, crowd will be None
+        """
+        img_id = self.ids[index]
+
+        ann_ids = self.coco.getAnnIds(imgIds=img_id)
+
+        # 'target' includes {'segmentation', 'area', iscrowd', 'image_id', 'bbox', 'category_id'}
+        target = self.coco.loadAnns(ann_ids)
+
+        # Separate out crowd annotations. These are annotations that signify a large crowd of objects,
+        # where there is no annotation for each individual object. When testing and training, treat these crowds as neutral.
+        crowd  = [x for x in target if ('iscrowd' in x and x['iscrowd'])]
+        target = [x for x in target if not ('iscrowd' in x and x['iscrowd'])]
+        num_crowds = len(crowd)
+
+        # Ensure that all crowd annotations are at the end of the array.
+        target += crowd
+        
+        # The split here is to have compatibility with both COCO2014 and 2017 annotations.
+        # In 2014, images have the pattern COCO_{train/val}2014_%012d.jpg, while in 2017 it's %012d.jpg.
+        # Our script downloads the images as %012d.jpg so convert accordingly.
+        file_name = self.coco.loadImgs(img_id)[0]['file_name']
+        if file_name.startswith('COCO'):
+            file_name = file_name.split('_')[-1]
+
+        path = osp.join(self.image_path, file_name)
+        assert osp.exists(path), 'Image path does not exist: {}'.format(path)
+        
+        img = cv2.imread(path)
+        height, width, _ = img.shape
+
+        if len(target) > 0:
+            # Pool all the masks for this image into one [num_objects, height, width] matrix
+            masks = [self.coco.annToMask(obj).reshape(-1) for obj in target]
+            masks = np.vstack(masks)
+            masks = masks.reshape(-1, height, width)
+
+        if len(target) > 0:
+            target = self.annotation_transform(target, width, height)
+
+        if self.augmentation is not None:
+            if len(target) > 0:
+                target = np.array(target)
+                img, masks, boxes, labels = self.augmentation(img, masks, target[:, :4], {'num_crowds': num_crowds, 'labels': target[:, 4]})
+            
+                # I stored num_crowds in labels so I didn't have to modify the entirety of augmentations
+                num_crowds = labels['num_crowds']
+                labels     = labels['labels']
+                
+                target = np.hstack((boxes, np.expand_dims(labels, axis=1)))
+            else:
+                img, _, _, _ = self.augmentation(img, np.zeros((1, height, width), dtype=np.float), np.array([[0, 0, 1, 1]]),
+                                                 {'num_crowds': 0, 'labels': np.array([0])})
+                masks = None
+                target = None
+
+        return torch.from_numpy(img).permute(2, 0, 1), target, masks, height, width, num_crowds
---
+++ data/config.py
@@ -0,0 +1,206 @@
+from modules.backbone import ResNetBackbone, DarkNetBackbone
+
+COLORS = ((244,  67,  54), (233,  30,  99), (156,  39, 176), (103,  58, 183),
+          ( 63,  81, 181), ( 33, 150, 243), (  3, 169, 244), (  0, 188, 212),
+          (  0, 150, 136), ( 76, 175,  80), (139, 195,  74), (205, 220,  57),
+          (255, 235,  59), (255, 193,   7), (255, 152,   0), (255,  87,  34),
+          (121,  85,  72), (158, 158, 158), ( 96, 125, 139))
+
+# These are in BGR and are for ImageNet
+MEANS = (103.94, 116.78, 123.68)
+STD   = (57.38, 57.12, 58.40)
+
+COCO_CLASSES = ('person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+                'train', 'truck', 'boat', 'traffic light', 'fire hydrant',
+                'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog',
+                'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe',
+                'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
+                'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat',
+                'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+                'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+                'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot',
+                'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch',
+                'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop',
+                'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven',
+                'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase',
+                'scissors', 'teddy bear', 'hair drier', 'toothbrush')
+
+COCO_LABEL_MAP = { 1:  1,  2:  2,  3:  3,  4:  4,  5:  5,  6:  6,  7:  7,  8:  8,
+                   9:  9, 10: 10, 11: 11, 13: 12, 14: 13, 15: 14, 16: 15, 17: 16,
+                  18: 17, 19: 18, 20: 19, 21: 20, 22: 21, 23: 22, 24: 23, 25: 24,
+                  27: 25, 28: 26, 31: 27, 32: 28, 33: 29, 34: 30, 35: 31, 36: 32,
+                  37: 33, 38: 34, 39: 35, 40: 36, 41: 37, 42: 38, 43: 39, 44: 40,
+                  46: 41, 47: 42, 48: 43, 49: 44, 50: 45, 51: 46, 52: 47, 53: 48,
+                  54: 49, 55: 50, 56: 51, 57: 52, 58: 53, 59: 54, 60: 55, 61: 56,
+                  62: 57, 63: 58, 64: 59, 65: 60, 67: 61, 70: 62, 72: 63, 73: 64,
+                  74: 65, 75: 66, 76: 67, 77: 68, 78: 69, 79: 70, 80: 71, 81: 72,
+                  82: 73, 84: 74, 85: 75, 86: 76, 87: 77, 88: 78, 89: 79, 90: 80}
+
+
+# ----------------------- CONFIG CLASS ----------------------- #
+class Config(object):
+    """
+    After implement this class, you can call 'cfg.x' instead of 'cfg['x']' to get a certain parameter.
+    """
+    def __init__(self, config_dict):
+        for key, val in config_dict.items():
+            self.__setattr__(key, val)
+
+    def copy(self, new_config_dict={}):
+        """
+        Copies this config into a new config object, making the changes given by new_config_dict.
+        """
+        ret = Config(vars(self))
+        
+        for key, val in new_config_dict.items():
+            ret.__setattr__(key, val)
+
+        return ret
+
+    def replace(self, new_config_dict):
+        """
+        Copies new_config_dict into this config object. Note: new_config_dict can also be a config object.
+        """
+        if isinstance(new_config_dict, Config):
+            new_config_dict = vars(new_config_dict)
+
+        for key, val in new_config_dict.items():
+            self.__setattr__(key, val)
+
+
+# ----------------------- DATASETS ----------------------- #
+dataset_base = Config({'name': 'COCO 2017',
+                       'train_images': '/home/feiyu/Data/coco2017/train2017/',
+                       'train_info':   '/home/feiyu/Data/coco2017/annotations/instances_train2017.json',
+                       'valid_images': '/home/feiyu/Data/coco2017/val2017/',
+                       'valid_info':   '/home/feiyu/Data/coco2017/annotations/instances_val2017.json',
+                       'class_names': COCO_CLASSES})
+
+# ----------------------- TRANSFORMS ----------------------- #
+resnet_transform = Config({'channel_order': 'RGB',
+                           'normalize': True,
+                           'subtract_means': False,
+                           'to_float': False})
+
+darknet_transform = Config({'channel_order': 'RGB',
+                            'normalize': False,
+                            'subtract_means': False,
+                            'to_float': True})
+
+# ----------------------- BACKBONES ----------------------- #
+resnet101_backbone = Config({'name': 'ResNet101',
+                             'path': 'resnet101_reducedfc.pth',
+                             'type': ResNetBackbone,
+                             'args': ([3, 4, 23, 3],),
+                             'transform': resnet_transform,
+                             'selected_layers': [1, 2, 3],
+                             'scales': [24, 48, 96, 192, 384],
+                             'aspect_ratios': [1, 1/2, 2],
+                             'use_square_anchors': True})    # This is for backward compatability with a bug.
+
+resnet50_backbone = resnet101_backbone.copy({'name': 'ResNet50',
+                                             'path': 'resnet50-19c8e357.pth',
+                                             'args': ([3, 4, 6, 3],)})
+
+darknet53_backbone = Config({'name': 'DarkNet53',
+                             'path': 'darknet53.pth',
+                             'type': DarkNetBackbone,
+                             'args': ([1, 2, 8, 8, 4],),
+                             'transform': darknet_transform,
+                             'selected_layers': [2, 3, 4],
+                             'scales': [24, 48, 96, 192, 384],
+                             'aspect_ratios': [1, 1/2, 2],
+                             'use_square_anchors': True})
+
+
+# ----------------------- FPN DEFAULTS ----------------------- #
+fpn_base = Config({
+    'num_features': 256,  # the number of features in each FPN layer
+    'interpolation_mode': 'bilinear',  # upsampling mode
+    'num_downsample': 2,  # The number of extra layers to be produced by downsampling starting at P5
+    # Whether to down sample with a 3x3 stride 2 conv layer instead of just a stride 2 selection
+    'use_conv_downsample': True,
+    # Whether to pad the pred layers with 1 on each side, this is just for backwards compatibility
+    'pad': 1})
+
+
+# ----------------------- YOLACT v1.0 CONFIGS ----------------------- #
+yolact_base_config = Config({
+    'name': 'yolact_base',
+    'dataset': dataset_base,
+    'num_classes': len(dataset_base.class_names) + 1,
+    'max_size': 550,    # image Size
+    'max_iter': 800000,
+    'backbone': resnet101_backbone,
+    # During training, first compute the maximum gt IoU for each prior.
+    # Then, for priors whose maximum IoU is over the positive threshold, marked as positive.
+    # For priors whose maximum IoU is less than the negative threshold, marked as negative.
+    # The rest are neutral ones and are not used to calculate the loss.
+    'positive_iou_threshold': 0.5,
+    'negative_iou_threshold': 0.4,
+    'bbox_alpha': 1.5,
+    'conf_alpha': 1,
+    # If less than 1, anchors treated as a negative that have a crowd iou over this threshold with
+    # the crowd boxes will be treated as a neutral.
+    'crowd_iou_threshold': 0.7,
+    # FPN Settings
+    'fpn': fpn_base,
+    # Freeze the backbone bn layer during training, other additional bn layers after the backbone will not be frozen.
+    'freeze_bn': True,
+
+    # Learning rate
+    'lr_steps': (280000, 600000, 700000, 750000),
+    'lr': 1e-3,
+    'momentum': 0.9,
+    'decay': 5e-4,
+    # the initial learning rate in the 'warm up' step, if > 0 then increase the lr linearly
+    # from warmup_init to lr each iter for until iters
+    'lr_warmup_init': 1e-4,
+    'lr_warmup_until': 500,
+
+    # The maximum number of detections for evaluation
+    'max_num_detections': 100,
+    'mask_alpha': 6.125,
+    'mask_proto_net': [(256, 3, {'padding': 1}), (256, 3, {'padding': 1}), (256, 3, {'padding': 1}), (None, -2, {}),
+                       (256, 3, {'padding': 1}), (32, 1, {})],
+    # Uses the same network format as mask_proto_net, except this time it's for adding extra head layers before the final
+    # prediction in prediction modules. If this is none, no extra layers will be added.
+    'extra_head_net': [(256, 3, {'padding': 1})],
+    'coef_dim': None,
+    'masks_to_train': 100,
+
+    # Add a 1x1 convolution directly to P3 to predict semantic segmentations for each of the 80 classes.
+    # This branch is only implemented during training and is just for multitask learning.
+    'use_semantic_segmentation_loss': True,
+    'semantic_segmentation_alpha': 1})
+
+
+yolact_im400_config = yolact_base_config.copy({
+    'name': 'yolact_im400',
+    'max_size': 400,
+    'backbone': yolact_base_config.backbone.copy({'scales': [17, 34, 69, 139, 279]})
+})
+
+
+yolact_im700_config = yolact_base_config.copy({
+    'name': 'yolact_im700',
+    'masks_to_train': 300,
+    'max_size': 700,
+    'backbone': yolact_base_config.backbone.copy({'scales': [30, 61, 122, 244, 488]})
+})
+
+
+yolact_darknet53_config = yolact_base_config.copy({'name': 'yolact_darknet53',
+                                                   'backbone': darknet53_backbone})
+
+
+yolact_resnet50_config = yolact_base_config.copy({'name': 'yolact_resnet50',
+                                                  'backbone': resnet50_backbone})
+
+def set_cfg(config_name: str):
+    """ Sets the active config. Works even if cfg is already imported! """
+    global cfg
+    cfg.replace(eval(config_name))
+
+# Default config
+cfg = yolact_base_config.copy()
